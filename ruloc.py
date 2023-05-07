706f98299582874b45bb344d5da0dd1be9e84eaf
Elizar54 <y.elizarev@g.nsu.ru>
2023-05-07 18:45:18 +0700

    new loc

--- ruloc.py
+++ ruloc.py
@@ -10,6 +10,15 @@ def loc(X):
     if X == 5:
         print(txteve5)
 
+def loc2(X):
+    txteve1 = "вы глупенький? \
+              1.да \
+              2.нет \
+              3.а? "
+    txteve2 = "текст2"
+    txteve3 = "текст3"
+    txt_sp = [0, texteve1, texteve2, texteve3]
+    print(txt_sp[X])
 
 txteve1 = "вы глупенький? \
           1.да \
