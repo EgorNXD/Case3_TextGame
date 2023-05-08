5c28f90ff2c7fbad96a08cdff5216f7d6219cd47
EgorNXD <e.nechaev@g.nsu.ru>
2023-05-08 16:24:35 +0700

    version 0.4

--- effects.py
+++ effects.py
@@ -1,9 +1,18 @@
 import ruloc as ru
-def eff(X, Y):
-  if X == 1:
-      if Y == 1:
-          print(ru.result1_1)
-      elif Y == 2:
-          print(ru.result1_2)
-      else:
-          print(ru.result1_3)
+
+
+def effct(x, y):
+    if x == 1:
+        if y == 1:
+            print(ru.result1_1)
+        elif y == 2:
+            print(ru.result1_2)
+        else:
+            print(ru.result1_3)
+
+
+"""
+Changes player resources and modifiers according to player's choices.
+x - event identifier 
+y - choice identifier
+"""
--- main.py
+++ main.py
@@ -1,23 +1,35 @@
 import ruloc as ru
 import effects as e
 
-def txt(X):
+
+def txt(x):
     txteve1 = ru.txteve1
     txteve2 = ru.txteve2
     txteve3 = ru.txteve3
     txt_sp = [0, txteve1, txteve2, txteve3]
-    print(txt_sp[X])
+    print(txt_sp[x])
 
 
-def event(X):
-    txt(X)
+"""
+Prints event flavor text and choices.
+x - event identifier
+"""
+
+
+def event(x):
+    txt(x)
     ans = int(input())
     if ans == 1:
-        e.eff(X, 1)
+        e.effct(x, 1)
     if ans == 2:
-        e.eff(X, 2)
+        e.effct(x, 2)
     if ans == 3:
-        e.eff(X, 3)
+        e.effct(x, 3)
 
+
+"""
+Event function. Calls txt function and effct function.
+x - event identifier
+"""
 
 event(1)
