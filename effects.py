7678133620528cc1b1f940827b046ea710be9afb
EgorNXD <e.nechaev@g.nsu.ru>
2023-05-15 01:42:11 +0700

    oh man3

--- effects.py
+++ effects.py
@@ -6,13 +6,10 @@ diplo = 100
 happiness = 100
 
 def effct(x, y):
-    if x == 1:
+    if x == 3:
         if y == 1:
-            print(ru.result1_1)
-        elif y == 2:
-            print(ru.result1_2)
-        else:
-            print(ru.result1_3)
+            print(ru.result1)
+
 
 def random_effct(x, y):
     global military, diplo, money, happiness
