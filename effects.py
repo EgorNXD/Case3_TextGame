acfdbffbe5ca5b307f9f0a81deb7864bb89719fb
Elizar54 <y.elizarev@g.nsu.ru>
2023-05-14 12:59:30 +0700

    added random event effects.

--- effects.py
+++ effects.py
@@ -10,6 +10,48 @@ def effct(x, y):
         else:
             print(ru.result1_3)
 
+def random_effct(x, y, military, diplo, happiness, money):
+    if x == 0:
+        if y == 1:
+            military -= 15
+        elif y == 2:
+            diplo += 10
+            money -= 15
+        else:
+            happiness -= 20
+    elif x == 1:
+        if y == 1:
+            military -= 15
+            happiness -= 20
+        else:
+            money -= 20
+            happiness += 20
+    elif x == 2:
+        happiness -= 10
+        money -= 15
+    elif x == 3:
+        money += 20
+    elif x == 4:
+        money -= 10
+    elif x == 5:
+        military -= 20
+    elif x == 6:
+        if y == 1:
+            happiness += 20
+        elif y == 2:
+            money -= 30
+            happiness += 30
+            diplo -= 10
+        else:
+            happiness -= 10
+    elif x == 7:
+        if y == 1:
+            money -= 10
+            happiness += 15
+        else:
+            happiness -= 15
+    return diplo, happiness, money, military
+
 
 """
 Changes player resources and modifiers according to player's choices.
